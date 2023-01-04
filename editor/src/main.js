78c2df01932266dae11807267da88f49f0af1851
Scottie Enriquez <scottie@enriquez.xxx>
2023-01-03 20:25:19 -0800

    Adding open file functionality

--- editor/src/main.js
+++ editor/src/main.js
@@ -1,22 +1,139 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow, dialog, Menu} = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+let mainWindow;
 
 function createWindow () {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({
+    mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
-    })
+    });
 
     // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000')
+    mainWindow.loadURL('http://localhost:3000');
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+    const isMac = process.platform === 'darwin';
+
+    const template = [
+        ...(isMac ? [{
+            label: app.name,
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services' },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideOthers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        }] : []),
+        {
+            label: 'File',
+            submenu: [
+                {
+                    label: 'Open File',
+                    accelerator: 'CmdOrCtrl+O',
+                    click() {
+                        openFile();
+                    }
+                },
+                {
+                    label: 'Open Directory'
+                }
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                ...(isMac ? [
+                    { role: 'pasteAndMatchStyle' },
+                    { role: 'delete' },
+                    { role: 'selectAll' },
+                    { type: 'separator' },
+                    {
+                        label: 'Speech',
+                        submenu: [
+                            { role: 'startSpeaking' },
+                            { role: 'stopSpeaking' }
+                        ]
+                    }
+                ] : [
+                    { role: 'delete' },
+                    { type: 'separator' },
+                    { role: 'selectAll' }
+                ])
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forceReload' },
+                { role: 'toggleDevTools' },
+                { type: 'separator' },
+                { role: 'resetZoom' },
+                { role: 'zoomIn' },
+                { role: 'zoomOut' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' }
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                ...(isMac ? [
+                    { type: 'separator' },
+                    { role: 'front' },
+                    { type: 'separator' },
+                    { role: 'window' }
+                ] : [
+                    { role: 'close' }
+                ])
+            ]
+        },
+        {
+            label: 'Developer',
+            submenu: [
+                {
+                    label: 'Open Developer Tools',
+                    accelerator: isMac ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+                    click() {
+                        mainWindow.webContents.openDevTools();
+                    }
+                }
+            ]
+        },
+        {
+            role: 'help',
+            submenu: [
+                {
+                    label: 'Learn More',
+                    click: async () => {
+                        const { shell } = require('electron')
+                        await shell.openExternal('https://electronjs.org')
+                    }
+                }
+            ]
+        }
+    ];
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 }
 
 // This method will be called when Electron has finished
@@ -41,3 +158,24 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+function openFile() {
+    dialog.showOpenDialog(mainWindow, {
+        properties: ['openFile'],
+        filters: [
+            {
+                name: 'Markdown',
+                extensions: ['md', 'mdx']
+            }
+        ]
+    }).then(function (file) {
+        if (!file) {
+            console.log('No files!')
+            return;
+        }
+        else {
+            const fileContent = fs.readFileSync(file.filePaths[0]).toString();
+            console.log(fileContent);
+        }
+    })
+
+}
