0a116822b17798aaf5cb57353c487c43627797ae
Scottie Enriquez <scottie@enriquez.xxx>
2023-01-15 21:16:48 -0800

    Adding basic editor styling

--- editor/src/App.js
+++ editor/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Component} from 'react';
 import Markdown from 'markdown-to-jsx';
 import AceEditor from 'react-ace';
+import styled from 'styled-components';
 import 'ace-builds/src-noconflict/mode-markdown';
 import 'ace-builds/src-noconflict/theme-dracula';
 import 'ace-builds/src-noconflict/ext-language_tools';
@@ -23,17 +24,52 @@ class App extends Component {
   render() {
     return (
         <div className="App">
-          <AceEditor
-              mode="markdown"
-              theme="dracula"
-              onChange={(updatedText) => { this.setState({loadedFile: updatedText}) }}
-              name="markdown-editor"
-              value={this.state.loadedFile}
-          />
-          <Markdown>{this.state.loadedFile}</Markdown>
+          <Split>
+              <CodeWindow>
+                <AceEditor
+                  mode="markdown"
+                  theme="dracula"
+                  onChange={(updatedText) => { this.setState({loadedFile: updatedText}) }}
+                  name="markdown-editor"
+                  value={this.state.loadedFile}
+                />
+              </CodeWindow>
+              <RenderedWindow>
+                  <Markdown>{this.state.loadedFile}</Markdown>
+              </RenderedWindow>
+          </Split>
         </div>
     );
   }
 }
 
 export default App;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+  h1, h2, h3, h4, h5, h6 {
+    color: #82d8d8;
+  }
+  h1 {
+    border-bottom: solid 3px #82d8d8;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #82d8d8; 
+  }
+`
